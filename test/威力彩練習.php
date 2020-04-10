d7be8241038dc804007239efb908485d3d5d1682
shuchang <dreamsky1208@gmail.com>
2020-04-10 11:32:08 +0800

    威力彩練習

--- test/威力彩練習.php
+++ test/威力彩練習.php
@@ -7,19 +7,44 @@ $num=[];
 for($i=0;$i<6;$i++){
     $num[]=rand(1,49);
 }
+echo "<pre>";print_r($num);echo "</pre>";
+for($i=0;$i<6;$i++){
+    for($j=0;$j<6;$j++){
+        if($i!=$j && $num[$j]==$num[$i]){
+            // unset($num[$j]);
+            // array_splice($num,$j,1);
+            $num[$j]=rand(1,49);
+        }
+    }
+}
 foreach($num as $n){
     echo $n . ",";
 }
-?>
 
+echo "<pre>";print_r($num);echo "</pre>";
+echo "<hr>";
+?>
 <?php
-$lottery=[];
-for($i=1;$i<50;$i++){
-    $lottery[]=$i;
+$num=[];
+while(count($num)<6){
+    if(count($num)>0){
+        $tmp=rand(1,38);
+
+        if(!in_array($tmp,$num)){
+            $num[]=$tmp;
+        }
+    }else{
+        $num[]=rand(1,38);
+    }
 }
-$xx=array_rand($lottery,6);
-for($j=0;$j<=6;$j++){
-    echo $lottery[$xx] . ',';
-}
+$num[]=rand(1,8);
+foreach($num as $n){
+    echo $n;
+    echo ",";
+    }
+    echo "<br>";
+    echo "<pre>";print_r($num);echo "</pre>";
+
+
 
 ?>
