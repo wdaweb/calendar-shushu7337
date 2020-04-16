762841d0f071ad436e4c6e82b2ed32bc85c177c1
shuchang <dreamsky1208@gmail.com>
2020-04-16 15:11:04 +0800

    月曆年曆製作

--- date.php
+++ date.php
@@ -74,3 +74,66 @@ for($i=0;$i<=30;$i++){
     echo date("Y-m-d",strtotime($today . "+" . $i . 'day')) . "<br>";
 }
 ?>
+<style>
+table{
+    border-collapse:collapse;
+}
+table td{
+    border:1px solid #ccc;
+    padding:5px;
+    text-align:center;
+}
+</style>
+<h4>月曆製作</h4>
+<div>年份:2020;</div>
+
+<?php
+$year="2020";
+for($Q=1;$Q<=12;$Q++){
+?>
+<div>月份:<?=$Q;?></div>
+<table>
+    <thead>
+        <tr>
+            <td>日</td>
+            <td>一</td>
+            <td>二</td>
+            <td>三</td>
+            <td>四</td>
+            <td>五</td>
+            <td>六</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $firstDay=date("$year-$Q-01");
+        $firstDayWeek=date("w",strtotime($firstDay));
+        $WeekDays=date("t",strtotime($firstDay));
+        echo $WeekDays;
+        echo "<br>";
+        echo $firstDayWeek;
+        echo "<br>";
+        if(date("Y",strtotime($firstDay)))
+        for($i=0;$i<6;$i++) {
+            echo "<tr>";
+                for($j=0; $j<7;$j++) {
+                    if($i==0 && $j<$firstDayWeek){
+                        echo "<td>";
+                        echo "</td>";
+                    }else{
+                        echo "<td>";
+                        $dd=$i*7+$j+1-$firstDayWeek;
+                        if($dd<=$WeekDays){
+                            echo $dd;
+                        }
+                    echo "</td>";
+                    }
+            }
+            echo "</tr>";
+        }
+        ?>
+    </tbody>
+</table>
+<?php
+}
+    ?>
