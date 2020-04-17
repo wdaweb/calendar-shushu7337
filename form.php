a21db9461930d395d9d62cd08ba107cfa5b118a0
shuchang <dreamsky1208@gmail.com>
2020-04-17 10:28:59 +0800

    BMI

--- form.php
+++ form.php
@@ -7,14 +7,28 @@ if(isset($_POST['weight']) && isset($_POST['height'])){
 
     $weight=$_POST['weight'];
     $height=$_POST['height'];
-    $BMI= $weight / ($height / 100 * $height / 100);
+    $BMI= round($weight / ($height / 100 * $height / 100),2);
     echo "輸入的身高是:".$height."<br>";
     echo "輸入的體重是:".$weight."<br>";
-    
     if(!empty($height) || !empty($weight) ){
-        echo "BMI為:". round($BMI,2) . "<br>";
+        echo "BMI為:". $BMI . "<br>";
     }else{
         echo "<span style='color:red'>請輸入正確身高體重值</span>";
+    }
+    //體位判定
+    echo "<br>";
+    if($BMI<18.5){
+        echo "體重過輕";
+    }else if ($BMI<24){
+        echo "健康體位";
+    }else if ($BMI<27){
+        echo "過重";
+    }else if ($BMI<30){
+        echo "輕度肥胖";
+    }else if ($BMI<35){
+        echo "中度肥胖";
+    }else{
+        echo "重度肥胖";
     }
 }
 
