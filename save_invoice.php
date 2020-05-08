f11d86d8f5ab09e5dd71289069398e464029d392
shuchang <dreamsky1208@gmail.com>
2020-05-08 14:23:49 +0800

    建立輸入發票的畫面及流程

---
+++ save_invoice.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo "period=".$_POST['period']."<br>";
+    echo "year=".$_POST['year']."<br>";
+    echo "code=".$_POST['code']."<br>";
+    echo "nubmer=".$_POST['nubmer']."<br>";
+    echo "spend=".$_POST['spend']."<br>";
+
+?>
