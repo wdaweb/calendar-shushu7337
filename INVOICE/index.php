1762c80d7d66f8b0bbf4d826f186282aa91c3777
shuchang <dreamsky1208@gmail.com>
2020-05-12 16:33:30 +0800

    layout test indexxx.php is orignal file

--- INVOICE/index.php
+++ INVOICE/index.php
@@ -1,95 +1,62 @@
 <link rel="stylesheet" href="./include/bootstrap.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>統一發票管理系統</title>
-    <link rel="stylesheet" href="./css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>統一發票管理系統</title>
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 
-<body>
-    
-        <form>
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="inputEmail4">Email</label>
-          <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-        </div>
-        <div class="form-group col-md-6">
-          <label for="inputPassword4">Password</label>
-          <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+<body class="d-flex justify-content-around align-items-center vw-100 flex-column" style="height: 100vh;">
+<form action="save_invoice.php" method="post"> 
+<div class="card my-5 mx-auto shadow alert-info border-warning" style="height: 300px;width: 500px;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img src="https://picsum.photos/90/300">
+      </div>
+      <div class="col-md-8">
+          <div class="form-group">
+            <div class="form-group col-md" >
+              <label for="inputYear">Year</label>
+              <select id="inputYear" class="form-control" name="year" >
+                <option selected>Choose...</option>
+                <option class="dropdown-item" value="2020" href="#">2020</option>
+                <option class="dropdown-item" value="2021" href="#">2021</option>
+                <option class="dropdown-item" value="2022" href="#">2022</option>
+              </select>
+            </div>
+            <div class="form-group col-md" >
+              <label for="inputMonth">Month</label>
+              <select id="inputMonth" class="form-control" name="period">
+                <option selected>Choose...</option>
+                <option class="dropdown-item" value="1">1-2</option>
+                <option class="dropdown-item" value="2">3-4</option>
+                <option class="dropdown-item" value="3">5-6</option>
+                <option class="dropdown-item" value="4">7-8</option>
+                <option class="dropdown-item" value="5">9-10</option>
+                <option class="dropdown-item" value="6">11-12</option>
+              </select>
+            </div>
+          </div>
+          <div class="col">獎號</div>
+          <input type="text" name="code" size="3px">
+          <input type="number" name="number">
+          <div class="col">花費</div>
+          <input type="number" name="expend">
+          <input type="submit" value="儲存">
         </div>
       </div>
-      <div class="form-group">
-        <label for="inputAddress">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-      </div>
-      <div class="form-group">
-        <label for="inputAddress2">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-      </div>
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="inputCity">City</label>
-          <input type="text" class="form-control" id="inputCity">
-        </div>
-        <div class="form-group col-md-4">
-          <label for="inputState">State</label>
-          <select id="inputState" class="form-control">
-            <option selected>Choose...</option>
-            <option>...</option>
-          </select>
-        </div>
-        <div class="form-group col-md-2">
-          <label for="inputZip">Zip</label>
-          <input type="text" class="form-control" id="inputZip">
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck">
-          <label class="form-check-label" for="gridCheck">
-            Check me out
-          </label>
-        </div>
-      </div>
-      <button type="submit" class="btn btn-primary">Sign in</button>
-    </form>
+    </div>
+  </div>
+  </div>
+  </form>
 
-
-
-
-
-
-
-
-
-    <form action="save_invoice.php" method="post">
-    期別:
-    <select name="period">
-        <option value="1">1,2月</option>
-        <option value="2">3,4月</option>
-        <option value="3">5,6月</option>
-        <option value="4">7,8月</option>
-        <option value="5">9,10月</option>
-        <option value="6">11,12月</option>
-    </select>    
-    年份:
-    <select name="year">
-        <option value="2020">2020</option>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
-    </select>
-    
-    獎號:
-        <input type="text" name="code">
-        <input type="number" name="number">
-    花費:
-        <input type="number" name="expend">
-        <input type="submit" value="儲存">
-    </form>
 </body>
 
 </html>
