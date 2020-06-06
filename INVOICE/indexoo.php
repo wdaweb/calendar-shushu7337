f9f02e63eeed90270848c5f63cdec26000eccd07
shu <dreamsky1208@gmail.com>
2020-06-06 19:53:00 +0800

    try 彈跳視窗

--- INVOICE/indexoo.php
+++ INVOICE/indexoo.php
@@ -1,7 +1,6 @@
 <link rel="shortcut icon" href="imgs/dot.ico" type="image/x-icon" />
 <script src="https://kit.fontawesome.com/7b3164b4a9.js" crossorigin="anonymous"></script>
-<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script> -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="include/bootstrap.css">
@@ -27,7 +26,7 @@ include "com/nav.php";
 <h1 class="top ">Receipt  Lottery</h1>
   <p class="h4">Welcome to this system !This page is for the matching invoice of the unified invoice, bless you to win the prize!</p>
 </header>
-<form id="info"> 
+<form action="save_invoice.php" method="post"> 
 <div class="card  mt-4 mx-auto shadow " style="height: 400px;width: 600px;">
     <div class="info row no-gutters">
       <div class="col-md">
@@ -66,7 +65,7 @@ include "com/nav.php";
             <div class="px-5 py-1">
               <div class="expend form-group col p-0 mb-1">花費</div>
                 <input class="ml-1 slt" type="number" name="expend" placeholder="請輸入金額" required="required">
-                <button class="float-right btn  btn-outline-light slt" id="save" data-toggle="modal" data-target="#exampleModal" >儲存</botton>
+                <input class="float-right btn  btn-outline-light slt" type="submit" value="儲存" >
               </div>
             </div>
           </div>
@@ -75,36 +74,7 @@ include "com/nav.php";
   </div>
   </div>
   </form>
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <a href="index.php">繼續新增</a>
-          <a href="list.php">前往發票列表</a>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  
 </body>
-<script>
-  $('#save').on('click',function(){
-    var form_data = $('#info').serialize();
-    //console.log(form_data);
-    $.post("save_invoice.php", form_data, function(result){
-      //資料已新增完成 這裡可再用來判斷資料是否新增成功
-    });
-  });
-</script>
 
 </html>
--- INVOICE/save_invoice.php
+++ INVOICE/save_invoice.php
@@ -1,30 +1,3 @@
-<link rel="shortcut icon" href="imgs/dot.ico" type="image/x-icon" />
-<script src="https://kit.fontawesome.com/7b3164b4a9.js" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="./include/bootstrap.css">
-<link rel="stylesheet" href="./css/stylein.css">
-<link rel="stylesheet" href="css/nav.css">
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<script src="https://kit.fontawesome.com/7b3164b4a9.js" crossorigin="anonymous"></script>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Receipt Lotter</title>
-  <link rel="stylesheet" href="./css/style.css">
-</head>
-
-<body >
-
-
-
-</body>
-
-</html>
 <?php
     include "com/base.php";
     // 可以將$_POST進來的資料設立新變數來做檢查，避免php混亂
@@ -56,7 +29,7 @@
     save("invoice",$data);
     // $res=save("invoice",$data);
   
-    to("index.php");
+    // to("index.php");
     
     
     // if($res==1){
