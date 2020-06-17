ce75b2efb56f6bf241281d0739a274c05cda22b4
shushu <dreamsky1208@gmail.com>
2020-06-17 23:18:19 +0800

    try all

--- INVOICE/award_all.php
+++ INVOICE/award_all.php
@@ -1,5 +1,5 @@
 <?php include_once "com/base.php";?>
-<link rel="shortcut icon" href="imgs/dot.ico" type="image/x-icon" />
+<!-- <link rel="shortcut icon" href="imgs/dot.ico" type="image/x-icon" />
 <script src="https://kit.fontawesome.com/7b3164b4a9.js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@700&display=swap" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
@@ -7,7 +7,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="include/bootstrap.css">
 <link rel="stylesheet" href="css/award.css">
-<script src="https://kit.fontawesome.com/7b3164b4a9.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/7b3164b4a9.js" crossorigin="anonymous"></script> -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,7 +20,7 @@
 
 <body>
     <?php
-include "com/nav.php";
+// include "com/nav.php";
 $monthStr=[
   '1'=>"1 - 2",
   '2'=>"3 - 4",
@@ -32,7 +32,6 @@ $monthStr=[
 ?>
 
     <?php 
-
     $award_type=[
         1=>["特別獎",1,8],
         2=>["特獎",2,8],
@@ -56,7 +55,22 @@ $monthStr=[
         8=>[200],
         9=>[200],
     ];
-    
+
+
+    ////////////////本期所有發票
+    $invoices=all("invoice",[
+        "year"=>$_POST['year'],
+        "period"=>$_POST['period'],
+        ]);
+    ////////////////本期開獎發票
+    $award_numbers=all("award_number",[
+        "year"=>$_POST['year'],
+        "period"=>$_POST['period'],
+        ]);
+
+
+
+
     if(isset($_POST['year'])){
       $year=$_POST['year'];
   }
@@ -64,8 +78,7 @@ $monthStr=[
       $period=$_POST['period'];
   }
     ?>
-    <div class="d-flex justify-content-center mt-5">
-        <table class="table text-center table-bordered justify-content-center table-striped table-dark table-hover">
+
             <tr>
                 <td>年份</td>
                 <td><?=$year;?></td>
@@ -82,79 +95,56 @@ $monthStr=[
                 
                     <?php
                     // 撈出本期中獎號碼
-                    $aw=1;
-                        
-                    $award_numbers=all("award_number",[
-                    "year"=>$_POST['year'],
-                    "period"=>$_POST['period'],
-                    ]);
-                    $t_num=[];
-                    foreach($award_numbers as $num){
-                        // echo $num ['number']."&emsp;";
-                        $t_num[]=$num ['number'];
-                    }
+                    // $t_num=[];
+                    // foreach($award_numbers as $num){
+                    //     $t_num[]=$num ['number'];
+                    // }
                   ?>
                 </td>
             </tr> -->
-            <tr>
-                <td>中獎發票號碼</td>
-                <td class="num">
-                    <?php
-                        $invoices=all("invoice",[
-                          "year"=>$_POST['year'],
-                          "period"=>$_POST['period'],
-                          ]);
-                          // 設立變數來接有?中獎
-                        $chk_num=0;
-                        echo "<hr>";
-                        for($aw>0;$aw<10;$aw++){
-                        foreach($invoices as $ins){
-                            foreach($t_num as $tn){
-                                $len=$award_type[$aw][2];
-                                $start=8-$len;
-                              // 針對增開六獎特別處理
-                              if($aw!=9){
-                                  $target_num=mb_substr($tn,$start,$len);
-                              }else{
-                                  // $tn 為增開六獎
-                                  $target_num=$tn;
-                              }
-                              if(mb_substr($ins['number'],$start,$len) == $target_num){
-                              $chk_num++;
-                              echo $ins['number']."&emsp;";
-                              }else{
-                                  // echo $ins['number']."再接再厲";
-                              }
-                            }//echo "<br>";
-                        }
-                    }
-                    ?>
-                </td>
-            </tr>
-            <tr>
-                <td>中獎筆數</td>
-                <td>
-                    <?php
-                    echo $chk_num;
-                    ?>
-                </td>
-            </tr>
-            <tr>
-                <td>中獎金額</td>
-                <td>
-                    <?php
-                    // echo $award_money[$aw][0];
-                    // echo $money=$chk_num*$award_money[$aw][0];
-                    ?>
-                </td>
-            </tr>
-        </table>
-    </div>
 
-    <?php
-        
-        ?>
+<?php
+    echo "<hr>";
+    
+    $chk_num=0;
+    $t_num=[];
+    $last[]="";
+    foreach($award_numbers as $num){
+        $t_num[]=$num ['number'];
+    }
+    
+    $aw=5;
 
-</body>
+    foreach($invoices as $ins){
+        foreach($t_num as $tn){
+            $len=$award_type[$aw][2];
+            $start=8-$len;
+          // 針對增開六獎特別處理
+          if($aw!=9){
+              $target_num=mb_substr($tn,$start,$len);
+          }else{
+              // $tn 為增開六獎
+              $target_num=$tn;
+          }
+          if(mb_substr($ins['number'],$start,$len) == $target_num){
+            $chk_num++;
+            echo $target_num;
+            $last[]=$target_num;
+        }
+    }
+}
 
-</html>
+echo count($last);
+echo "<pre>";
+print_r($last);
+echo "</pre>";
+echo $chk_num;
+
+
+
+
+?>
+
+
+
+
